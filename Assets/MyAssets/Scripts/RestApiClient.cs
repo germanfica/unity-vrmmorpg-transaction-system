5b6a35c88204e6938a6e5c23db442fda6aebe789
German Fica <germafica@gmail.com>
2022-04-27 01:20:29 -0300

    Add RestApiClient singleton variable

--- Assets/MyAssets/Scripts/RestApiClient.cs
+++ Assets/MyAssets/Scripts/RestApiClient.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RestApiClient : MonoBehaviour
 {
+    // singleton for easier access
+    public static RestApiClient singleton;
+
     // Start is called before the first frame update
     void Start()
     {
