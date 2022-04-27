1ca3fb39bccd4c2163ef7dd6fe08704dd5935bd4
German Fica <germafica@gmail.com>
2022-04-27 01:34:46 -0300

    Add PlayerCharacter request web model

--- Assets/MyAssets/Scripts/RestApiClient.cs
+++ Assets/MyAssets/Scripts/RestApiClient.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,16 @@ public class RestApiClient : MonoBehaviour
 
     // api url
     public string serverUrl;
+
+    [Serializable]
+    class PlayerCharacter
+    {
+        public string id;
+        public string name;
+        public int level;
+        public int health;
+        public int experience;
+    }
 
     // Start is called before the first frame update
     void Start()
