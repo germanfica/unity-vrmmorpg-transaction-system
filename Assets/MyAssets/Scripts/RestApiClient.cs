49039c51bc5245f26793d75e9abd7a87472032af
German Fica <germafica@gmail.com>
2022-04-27 01:23:47 -0300

    Add server url variable

--- Assets/MyAssets/Scripts/RestApiClient.cs
+++ Assets/MyAssets/Scripts/RestApiClient.cs
@@ -7,6 +7,9 @@ public class RestApiClient : MonoBehaviour
     // singleton for easier access
     public static RestApiClient singleton;
 
+    // api url
+    public string serverUrl;
+
     // Start is called before the first frame update
     void Start()
     {
