c7f161d5c09a80d26234200c31d3ce4872c56bd9
German Fica <germafica@gmail.com>
2022-04-27 01:27:42 -0300

    Add get unity web request example

--- Assets/MyAssets/Scripts/RestApiClient.cs
+++ Assets/MyAssets/Scripts/RestApiClient.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class RestApiClient : MonoBehaviour
 {
@@ -13,12 +14,43 @@ public class RestApiClient : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // A correct website page.
+        StartCoroutine(GetRequest(serverUrl));
+
+        // A non-existing page.
+        StartCoroutine(GetRequest("https://error.html"));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // https://docs.unity3d.com/ScriptReference/Networking.UnityWebRequest.Get.html
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
     }
 }
