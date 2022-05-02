ee6b6216985d2067e7126c054463d1c4c3aa1ac8
German Fica <germafica@gmail.com>
2022-05-02 18:28:01 -0300

    Add UI

---
+++ Assets/MyAssets/Scripts/Scroller.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ScrollRect))]
+public class Scroller : MonoBehaviour
+{
+
+}
